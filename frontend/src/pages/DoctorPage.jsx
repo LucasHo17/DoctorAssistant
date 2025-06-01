4df0a74fe4b3674f0e5ce6d4e5443240527c07f9
JunHo <dungho@umass.edu>
2025-06-01 14:46:49 +0700

    update doctor page

--- frontend/src/pages/DoctorPage.jsx
+++ frontend/src/pages/DoctorPage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import './DoctorPage.css';
 import { ImPhoneHangUp } from 'react-icons/im';
 import { TiMicrophone, } from 'react-icons/ti';
