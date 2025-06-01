51e031f0d7f64dbf6c8b03b0e8070e7c0c1fc3b6
JunHo <dungho@umass.edu>
2025-06-01 22:23:23 +0700

    up frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL:  'https://doctorassistant-3.onrender.com',
+  baseURL:  'https://doctorassistant-backend.onrender.com/',
 });
 
 instance.interceptors.request.use((config) => {
