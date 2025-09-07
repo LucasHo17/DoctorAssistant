9ee090045370f738144d73275fecdb04f2b52bfa
JunHo <dungho@umass.edu>
2025-09-06 21:27:07 -0400

    fixing frontend endpoint so that it fits with docker configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000'
-,
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? '/api',
 });
 
 instance.interceptors.request.use((config) => {
